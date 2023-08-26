6a98691c69f41b503b4fed8e26951079453f4196
dtyohemba <dtyohemba@getcarbon.co>
2023-08-26 09:11:53 +0100

    removing recursion from queue

--- src/workers/sellConsumer.ts
+++ src/workers/sellConsumer.ts
@@ -1,29 +1,29 @@
 import { receiveMessages, deleteMessage } from '../messaging/AwsQueuingService';
 import { sellItem } from '../services/itemService';
 
-
 const processSellRequests = async (queueUrl: string) => {
   try {
-    console.log("Waiting for Messages!!")
-    const maxMessages = 10;
-    const messages = await receiveMessages(queueUrl, maxMessages);
+    while (true) {
+      console.log("Waiting for Messages!!");
+      const maxMessages = 10;
+      const messages = await receiveMessages(queueUrl, maxMessages);
 
-    for (const message of messages) {
-      const { item, quantity } = JSON.parse(message.Body);
-      console.log(`Received Message ${item} - ${quantity}`)
+      for (const message of messages) {
+        const { item, quantity } = JSON.parse(message.Body);
+        console.log(`Received Message ${item} - ${quantity}`);
 
-      // Call the sellItem function or your desired logic
-      await sellItem(item, quantity);
+        // Call the sellItem function or your desired logic
+        await sellItem(item, quantity);
 
-      // Delete the processed message from the queue
-      await deleteMessage(queueUrl, message.ReceiptHandle);
+        // Delete the processed message from the queue
+        await deleteMessage(queueUrl, message.ReceiptHandle);
+      }
     }
   } catch (error) {
     console.error('Error processing sell requests:', error);
     throw error;
-  }finally{
-    processSellRequests(process.env.queueUrl || 'https://sqs.us-west-2.amazonaws.com/334236250727/elite-dev')
   }
 };
 
-processSellRequests(process.env.queueUrl || 'https://sqs.us-west-2.amazonaws.com/334236250727/elite-dev')
+// Start processing sell requests
+processSellRequests(process.env.queueUrl || 'https://sqs.us-west-2.amazonaws.com/334236250727/elite-dev');
